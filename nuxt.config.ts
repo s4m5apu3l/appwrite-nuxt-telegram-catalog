d615de1a49eefbaaaef6ad7b593d344488d5e440
s4m5apu3l <asperatuz@yandex.ru>
2024-01-22 22:21:39 +0700

    init: appwrite, tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  ssr: false,
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  modules: ['nuxt-appwrite', '@nuxtjs/tailwindcss'],
+  appwrite: {
+    endpoint: 'https://cloud.appwrite.io/v1',
+    // project: 'nuxt-playground',
+  },
+  tailwindcss: {
+    cssPath: false,
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    exposeLevel: 4,
+    config: {},
+    injectPosition: 'first',
+    viewer: true,
+  }
 })
